543d2f4e9be4e5665ad4bb9783a5514285196bf6
nitesh-softions <nitesh.softieons@gmail.com>
2024-10-29 16:33:37 +0530

    token error solved

--- src/routes/route.jsx
+++ src/routes/route.jsx
@@ -1,12 +1,9 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
-import { decryptData, getCookie } from "../utils/CommonFunctions";
+import { getCookie } from "../utils/CommonFunctions";
 
 const Authmiddleware = (props) => {
-  const token = decryptData(getCookie("token"));
-
-  console.log('getCookie("token"): ', getCookie("token"));
-  console.log('token: ', token);
+  const token = getCookie("token");
   
   if (!token) {
     return (
