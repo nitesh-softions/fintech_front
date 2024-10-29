109fa4f2217eef253cc03e65396231d3d019b99c
nitesh-softions <nitesh.softieons@gmail.com>
2024-10-29 16:18:49 +0530

    - login functionality added through api

--- src/routes/route.jsx
+++ src/routes/route.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
+import { decryptData, getCookie } from "../utils/CommonFunctions";
 
 const Authmiddleware = (props) => {
-  // if (!localStorage.getItem("authUser")) {
-  //   return (
-  //     <Navigate to={{ pathname: "/login", state: { from: props.location } }} />
-  //   );
-  // }
+  const token = decryptData(getCookie("token"));
+
+  console.log('getCookie("token"): ', getCookie("token"));
+  console.log('token: ', token);
+  
+  if (!token) {
+    return (
+      <Navigate to={{ pathname: "/login", state: { from: props.location } }} />
+    );
+  }
   return <React.Fragment>{props.children}</React.Fragment>;
 };
 
